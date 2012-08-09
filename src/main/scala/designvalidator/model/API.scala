a679b00f356cd3a7266550f2972d0abd9a77ca50
Michal Jurczynski <michal.jurczynski@gmail.com>
2012-08-09 23:17:11 +0100

    Model extraction with field and method dependencies.

--- src/main/scala/designvalidator/model/API.scala
+++ src/main/scala/designvalidator/model/API.scala
@@ -1,4 +1,19 @@
 package designvalidator.model
 
-case class ClassModel(name: String, constructors: Seq[MethodModel], methods: Seq[MethodModel])
-case class MethodModel(name: String, args: Seq[String])
+case class ClassModel(name: String,
+  methods: Seq[MethodModel],
+  fields: Seq[FieldModel])
+
+case class MethodModel(name: String, args: Seq[String],
+  exceptions: Seq[String],
+  returnType: String,
+  fieldDependencies: Seq[FieldDependency],
+  methodDependencies: Seq[MethodDependency],
+  creates: Seq[String])
+
+case class FieldModel(name: String)
+
+case class FieldDependency(static: Boolean, owner: String, name: String,
+    read: Boolean)
+    
+case class MethodDependency(static: Boolean, owner: String, name: String)
--- src/main/scala/designvalidator/model/ModelExtractor.scala
+++ src/main/scala/designvalidator/model/ModelExtractor.scala
@@ -8,7 +8,7 @@ import org.objectweb.asm._
 class ModelExtractor {
   def readJar(file: File): Seq[ClassModel] = {
     val zip = new ZipFile(file)
-    zip.entries().toList.map(e => { println("r:" + e.getName()); e }).filter(_.getName().endsWith(".class")).map(entry => {
+    zip.entries().toList.filter(_.getName().endsWith(".class")).map(entry => {
       val reader = new ClassReader(zip.getInputStream(entry))
       val builder = new ClassModelBuilder()
       reader.accept(builder, 0)
@@ -18,6 +18,7 @@ class ModelExtractor {
 
   private class ClassModelBuilder extends ClassVisitor {
     var methods = Seq[MethodModel]()
+    var fields = Seq[FieldModel]()
     var name = ""
     def visit(
       version: Int,
@@ -42,18 +43,80 @@ class ModelExtractor {
       name: String,
       desc: String,
       signature: String,
-      value: Object): FieldVisitor = null
+      value: Object): FieldVisitor = {
+      fields = fields :+ FieldModel(name)
+      null
+    }
     def visitMethod(
       access: Int,
       name: String,
       desc: String,
       signature: String,
       exceptions: Array[String]): MethodVisitor = {
-      methods = methods :+ MethodModel(name, Seq(signature))
-      null
+      new MethodModelBuilder(name, exceptions)
     }
     def visitEnd() {}
-    def apply() = ClassModel(name, Seq(), methods)
-  }
+    def apply() = ClassModel(name, methods, fields)
 
+    private class MethodModelBuilder(name: String, exceptions: Array[String])
+      extends MethodVisitor {
+      var fieldDependencies = Seq[FieldDependency]()
+      var methodDependencies = Seq[MethodDependency]()
+      var creates = Seq[String]()
+      def visitAnnotationDefault(): AnnotationVisitor = null
+      def visitAnnotation(desc: String, visible: Boolean): AnnotationVisitor = null
+      def visitParameterAnnotation(
+        parameter: Int,
+        desc: String,
+        visible: Boolean): AnnotationVisitor = null
+      def visitAttribute(attr: Attribute) {}
+      def visitCode() {}
+      def visitFrame(
+        `type`: Int,
+        nLocal: Int,
+        local: Array[Object],
+        nStack: Int,
+        stack: Array[Object]) {}
+      def visitInsn(opcode: Int) {}
+      def visitIntInsn(opcode: Int, operand: Int) {}
+      def visitVarInsn(opcode: Int, `var`: Int) {}
+      def visitTypeInsn(opcode: Int, `type`: String) {
+        if (opcode == Opcodes.NEW) {
+          creates = creates :+ `type`
+        }
+      }
+      def visitFieldInsn(opcode: Int, owner: String, name: String, desc: String) {
+        val static = opcode == Opcodes.GETSTATIC || opcode == Opcodes.PUTSTATIC
+        val read = opcode == Opcodes.GETSTATIC || opcode == Opcodes.GETFIELD
+        fieldDependencies = fieldDependencies :+ FieldDependency(static, owner,
+            name, read)
+      }
+      def visitMethodInsn(opcode: Int, owner: String, name: String, desc: String) {
+        methodDependencies = methodDependencies :+ MethodDependency(false,
+            owner, name)
+      }
+      def visitJumpInsn(opcode: Int, label: Label) {}
+      def visitLabel(label: Label) {}
+      def visitLdcInsn(cst: Object) {}
+      def visitIincInsn(`var`: Int, increment: Int) {}
+      def visitTableSwitchInsn(min: Int, max: Int, dflt: Label, labels: Array[Label]) {}
+      def visitLookupSwitchInsn(dflt: Label, keys: Array[Int], labels: Array[Label]) {}
+      def visitMultiANewArrayInsn(desc: String, dims: Int) {}
+      def visitTryCatchBlock(start: Label, end: Label, handler: Label, `type`: String) {}
+      def visitLocalVariable(
+        name: String,
+        desc: String,
+        signature: String,
+        start: Label,
+        end: Label,
+        index: Int) {}
+      def visitLineNumber(line: Int, start: Label) {}
+      def visitMaxs(maxStack: Int, maxLocals: Int) {}
+      def visitEnd() {
+        methods = methods :+ MethodModel(name, Seq(), exceptions, null,
+            fieldDependencies, methodDependencies, creates)
+      }
+    }
+
+  }
 }
