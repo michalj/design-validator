0a1ef6c37432022900eba820e48bcad4d30d4219
mateuszj <mateuszjurczynski@gmail.com>
2012-08-10 01:07:03 +0200

    escaping id and labels

--- src/main/scala/designvalidator/graph/GraphVizGraphWriter.scala
+++ src/main/scala/designvalidator/graph/GraphVizGraphWriter.scala
@@ -6,18 +6,18 @@ class GraphVizGraphWriter(output: Writer) extends IGraphWriter {
   val out = new BufferedWriter(output)
   
   def begin {
-    out.write("digraph {")
+    //out.write("digraph {")
   }
   
   def addVerticle(id: String, label: String, color: String = "black",
       bgcolor: String = "white") {
-	  out.write(id + "[label=\"" + label + "\", fontcolor=" + color + ",color=" + bgcolor + "];")
+	  out.write("\"" + id + "\" [label=\"" + label + "\", fontcolor=" + color + ",color=" + bgcolor + "];")
   }
   def addEdge(fromId: String, toId: String, label: String) {
-    out.write(fromId + "->" + toId + ";")
+    out.write("\"" + fromId + "\"->\"" + toId + "\";")
   }
   
   def end {
-    out.write("}")
+    //out.write("}")
   }
 }
