841c9816a9a218629bdee58ede90c993379752bc
mateuszj <mateuszjurczynski@gmail.com>
2012-08-10 00:49:56 +0200

    dot language support

--- src/main/scala/designvalidator/graph/IGraphWriter.scala
+++ src/main/scala/designvalidator/graph/IGraphWriter.scala
@@ -4,4 +4,7 @@ trait IGraphWriter {
   def addVerticle(id: String, label: String, color: String = "black",
       bgcolor: String = "white")
   def addEdge(fromId: String, toId: String, label: String)
+  
+  def begin
+  def end
 }
