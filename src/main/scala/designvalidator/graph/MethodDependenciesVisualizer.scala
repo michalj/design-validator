26ef4968ce46c2ff7a790ac4ffa4afd2d348daa7
Michal Jurczynski <michal.jurczynski@gmail.com>
2012-08-13 09:18:16 +0100

    Stub for method-level dependencies visualizer.

---
+++ src/main/scala/designvalidator/graph/MethodDependenciesVisualizer.scala
@@ -0,0 +1,7 @@
+package designvalidator.graph
+
+import designvalidator.model._
+
+object MethodDependenciesVisualizer extends IVisualizer {
+  def apply(model: Seq[ClassModel]): Graph = null // TODO
+}
